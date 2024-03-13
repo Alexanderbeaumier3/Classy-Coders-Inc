b8ead334fbb960dc3c098cb068b645b0863cd0b9
Cedrick Catalan <ccata002@gmail.com>
2024-03-13 16:32:01 -0400

    added the rest of the stuff

--- classes/Employees.js
+++ classes/Employees.js
@@ -2,33 +2,69 @@ class Employees {
     #salary
     #isHired
 
+    static allEmployees=[];
+    
     constructor(name, position, salary) {
-    this.name = name;
-    this.position = position
-    this.#salary = salary
-    this.#isHired = true;
-}
-
-getSalary() {
-    return this.#salary
-}
-
-setSalary(amount) {
-    this.#salary = amount;
-}
-
-getStatus() {
-    return this.#isHired
-}
-
-setStatus(command) {
-    if (command === 'hire') {
+        this.name = name;
+        this.position = position
+        this.#salary = salary
         this.#isHired = true;
-    } else if (command === 'fire') {
-        this.#isHired = false;
+        Employees.allEmployees.push(this)
     }
-}
 
+    getSalary() {
+        return this.#salary
+    }
+
+    setSalary(amount) {
+        if(amount<0){
+            throw new Error("Salary cannot be negative")
+        }
+        this.#salary = amount;
+    }
+
+    getStatus() {
+        return this.#isHired
+    }
+
+    setStatus(command) {
+        if (command === 'hire') {
+            this.#isHired = true;
+        } else if (command === 'fire') {
+            this.#isHired = false;
+        }
+    }
+
+    static getEmployees(){
+        return Employees.allEmployees
+    }
+
+    static getTotalPayroll(){
+        let total = 0;
+        for(let i=0;i<Employees.allEmployees.length;i++){
+            const employee = Employees.allEmployees[i];
+            total+=employee.getSalary();
+        }
+        return total;
+
+        
+        // Array.forEach()
+        // let total = 0;
+        // Employees.allEmployees.forEach(employee=>{
+        //     total+=employee.getSalary();
+        // })
+        // return total
+
+        // Array.reduce()
+        // return Employees.allEmployees.reduce((total,employee)=>{
+        //     return total+employee.getSalary();
+        // },0)
+    }
+
+    promote(position, salary){
+        this.position = position;
+        this.setSalary(salary);
+    }
     
 }
 
--- classes/SalesPerson.js
+++ classes/SalesPerson.js
@@ -1,5 +1,22 @@
-class SalesPerson {
+const {Employees} = require('./Employees')
 
+class SalesPerson extends Employees{
+    #totalSales
+    constructor(name,position,salary,clients){
+        super(name,position,salary);
+        this.clients=clients;
+        this.#totalSales=0;
+    }
+    getSalesNumber(){
+        return this.#totalSales;
+    }
+    makeSale(amount){
+        this.#totalSales+=amount;
+    }
+
+    findClient(client){
+        return this.clients[client];
+    }
 
 
     
